8bea5f01ad8deadb3e4c5fa56676e17804a1104a
timothydnguyen <nguyen.d.timothy@gmail.com>
2018-04-15 23:44:22 -0700

    updated installation and setup instructions for mac`

--- installation_and_setup/install_psql.sh
+++ installation_and_setup/install_psql.sh
@@ -1,4 +1,5 @@
-/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-brew upgrade
-brew install postgresql
-pg_ctl -D /usr/local/var/postgres start
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)" # get homebrew (package manager)
+brew upgrade # upgrade homebrew
+brew install postgresql # install postgresql thru homebrew
+pg_ctl -D /usr/local/var/postgres start # start server
+brew services start postgresql # have the postgresql server start at computer startup
