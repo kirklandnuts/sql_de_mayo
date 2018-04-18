5fb0c8ccbccfaade7e2f19ddbfce22c4d10d60a6
Timothy Nguyen <nguyen.d.timothy@gmail.com>
2018-04-18 10:27:39 -0700

    Update jupyter_setup.sh

--- installation_and_setup/jupyter_setup.sh
+++ installation_and_setup/jupyter_setup.sh
@@ -1,4 +1,8 @@
 cd ..;
+pip3 install virtualenv;
 virtualenv -p python3 venv;
 source venv/bin/activate;
 pip install -r requirements.txt;
+git clone https://github.com/bgschiller/postgres_kernel;
+python setup.py build;
+python setup.py install;
