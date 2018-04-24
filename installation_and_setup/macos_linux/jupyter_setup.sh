3cb951216e633eb7fd6275121af5648893680ae8
timothydnguyen <nguyen.d.timothy@gmail.com>
2018-04-23 19:48:38 -0700

    updated jupyter instructions

--- installation_and_setup/macos_linux/jupyter_setup.sh
+++ installation_and_setup/macos_linux/jupyter_setup.sh
@@ -1,12 +1,12 @@
-cd ..;
+cd ../..;
 pip3 install virtualenv;
 virtualenv -p python3 venv;
 source venv/bin/activate;
-pip install jupyter
+pip install jupyter;
 # pip install -r requirements.txt; WILL UPLOAD LATER- psycopg2 and tabulate should be the only dependencies for postgres_kernel, but check pip logging if having errors to check for additional dependencies that i'm missing
-pip install psycopg2
-pip install tabulate
+pip install psycopg2;
+pip install tabulate;
 git clone https://github.com/bgschiller/postgres_kernel;
-cd postgres_kernel
+cd postgres_kernel;
 python setup.py build;
 python setup.py install;
