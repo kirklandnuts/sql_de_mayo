a359d2d02b59fb1c15afa1fb214ea40fb7a4cf05
timothydnguyen <nguyen.d.timothy@gmail.com>
2018-04-23 20:31:10 -0700

    added python install

--- installation_and_setup/macos_linux/jupyter_setup.sh
+++ installation_and_setup/macos_linux/jupyter_setup.sh
@@ -1,13 +1,10 @@
 cd ../..;
-if command -v python3 &>/dev/null; then
-    echo Python 3 is installed;
-else
-    /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)";
-    brew install python3;
-fi
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)";
+
+brew install python3;
 pip3 install --upgrade pip;
 pip3 install virtualenv;
-virtualenv -p python3 venv;
+virtualenv -p /usr/local/Cellar/python/3.6.5/bin/python3 venv;
 source venv/bin/activate;
 pip install jupyter;
 pip install psycopg2;
