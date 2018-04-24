9ed7dfbea8a712795b4fa6f622b0d63c6d3ef57d
timothydnguyen <nguyen.d.timothy@gmail.com>
2018-04-23 20:09:28 -0700

    added python install to jupyter setup

--- installation_and_setup/macos_linux/jupyter_setup.sh
+++ installation_and_setup/macos_linux/jupyter_setup.sh
@@ -1,9 +1,15 @@
 cd ../..;
+if command -v python3 &>/dev/null; then
+    echo Python 3 is installed;
+else
+    /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)";
+    brew install python3;
+fi
+pip3 install --upgrade pip;
 pip3 install virtualenv;
 virtualenv -p python3 venv;
 source venv/bin/activate;
 pip install jupyter;
-# pip install -r requirements.txt; WILL UPLOAD LATER- psycopg2 and tabulate should be the only dependencies for postgres_kernel, but check pip logging if having errors to check for additional dependencies that i'm missing
 pip install psycopg2;
 pip install tabulate;
 git clone https://github.com/bgschiller/postgres_kernel;
